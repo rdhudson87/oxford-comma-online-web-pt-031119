bb23f4b64b7d602f32cde0865ae66712713d2af5
Ricardo H <ricardo.hudson923@gmail.com>
2019-03-20 21:40:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,23 +1,5 @@
 def oxford_comma(array)
 ["kiwi"].join
-
-end
-def oxford_comma(array)
 ["kiwi","durian"].join(" and ")
 end
 
-def oxford_comma(array)
-["kiwi","durian","starfruit"].join(" and ")
-end
-
-def oxford_comma(array)
-["kiwi","durian","starfruit","mangos","dragon fruits"].join(" and ")
-end
-
-def oxford_comma(array)
-["kiwi","durian","starfruit","mangos","dragon fruits"].join(" and ")
-end
-
-def oxford_comma(array)
-["kiwi","durian","starfruit","mangos","dragon fruits","lychees", "pomelos"].join(" and ")
-end
